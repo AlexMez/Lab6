b7eb257a9126d33c94a9621a35c457ea82696968
Alex Meza <ajex995@gmail.com>
2017-04-15 14:42:23 -0500

    added option to contine

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -10,46 +10,116 @@ namespace Lab6
     {
         static void Main(string[] args)
         {
-            bool run = true;
-            while (run)
+            bool displayMenu = true;
+            while (displayMenu)
             {
-
-                Char[] vowels = { 'e', 'u', 'i', 'o', 'a', };
-                Console.WriteLine("enter a word");
-
-                string Mystring = Console.ReadLine();
-
-                Console.WriteLine("------------------------------");
-
-                //string Mystring2 = "apples";
-
-                Mystring.IndexOfAny(vowels);
-
-                //Console.WriteLine((Mystring.IndexOfAny(vowels)));
-
-                if ((Mystring.IndexOfAny(vowels) > 0))
-                {
-
-                    int FirstVowel = Mystring.IndexOfAny(vowels);
-
-                    Mystring.Substring(FirstVowel);
-                    string FirstConst = Mystring.Substring(0, FirstVowel);
-
-                    string MyStringChange = Mystring.Substring(FirstVowel) + FirstConst + "ay";
-                    //Console.WriteLine("the word starts with a Consonant");
-                    Console.WriteLine(MyStringChange);
-                }
-                else
-                {
-                    string ChangeString = Mystring;
-
-                    ChangeString = Mystring + "way";
-
-                    Console.WriteLine(ChangeString);
-                }
-
-
+               
+                displayMenu = MainMenu();
+                
             }
         }
+
+        static void PigLatin()
+        {
+
+            Char[] vowels = { 'e', 'u', 'i', 'o', 'a', };
+            Console.WriteLine("enter the word");
+
+            string WordInput = Console.ReadLine();
+
+            Console.Clear();
+
+            string Mystring = WordInput.ToLower();
+
+            Console.WriteLine("------------------------------");
+
+            Mystring.IndexOfAny(vowels);
+
+            if ((Mystring.IndexOfAny(vowels) > 0))
+            {
+
+                int FirstVowel = Mystring.IndexOfAny(vowels);
+
+                Mystring.Substring(FirstVowel);
+                string FirstConst = Mystring.Substring(0, FirstVowel);
+
+                string MyStringChange = Mystring.Substring(FirstVowel) + FirstConst + "ay";
+                //Console.WriteLine("the word starts with a Consonant");
+                Console.WriteLine("In Pig Latin {0} would be: {1} ", WordInput, MyStringChange);
+            }
+            else
+            {
+                string ChangeString = Mystring;
+
+                ChangeString = Mystring + "way";
+
+                Console.WriteLine("In Pig Latin {0} would be: {1} ", WordInput, ChangeString);
+            }
+            Console.WriteLine();
+            
+        }
+        public static int GetValidInteger()
+        {
+            int input;
+
+            while (!int.TryParse(Console.ReadLine(), out input))
+            {
+                Console.WriteLine("This is not an option");
+            }
+            return input;
+        }
+        public static int RangeValidator(int min, int max)
+
+        {
+
+            int input;
+
+            //Console.WriteLine($"Please enter an Integer between {min} and {max}");
+
+            input = GetValidInteger();
+
+            while (input < min || input > max)
+
+            {
+
+                Console.WriteLine("this is not an option");
+
+                input = GetValidInteger();
+
+            }
+
+            return input;
+
+
+
+        }
+        public static bool MainMenu()
+        {
+            Console.WriteLine("Choose an option");
+            Console.WriteLine("1) Translate a word into Pig Latin");
+            Console.WriteLine("2) Quit");
+            
+
+            int userInput = RangeValidator(1, 2);
+
+            if (userInput == 1)
+            {
+                PigLatin();
+                return true;
+            }
+            else if (userInput == 2)
+            {
+                Console.WriteLine("option 2");
+                return false;
+            }
+            
+            else
+            {
+                return true;
+            }
+
+        }
+
+
     }
 }
